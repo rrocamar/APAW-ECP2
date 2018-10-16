c1a8f3c90d7d645aef9eaeaae284fe724a5c57db
rroca <r.roca@alumnos.upm.es>
2018-10-16 22:04:50 +0200

    Creo la clase concreta DaoFactoryMemory.

---
+++ src/main/java/api/daos/memory/DaoFactoryMemory.java
@@ -0,0 +1,17 @@
+package api.daos.memory;
+
+import api.daos.DaoFactory;
+import api.daos.EmpleadoDao;
+
+public class DaoFactoryMemory extends DaoFactory {
+
+    private EmpleadoDao empleadoDao;
+
+    @Override
+    public EmpleadoDao getEmpleadoDao() {
+        if (this.empleadoDao == null) {
+            this.empleadoDao = new EmpleadoDaoMemory();
+        }
+        return this.empleadoDao;
+    }
+}
