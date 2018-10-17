4325709372b73fe365b6bf45c94c7cf92bc7d8bb
rroca <r.roca@alumnos.upm.es>
2018-10-17 22:30:30 +0200

    Refactoring

--- src/test/java/EmpleadoIT.java
+++ src/test/java/EmpleadoIT.java
@@ -8,8 +8,6 @@ import http.HttpRequest;
 import http.HttpResponse;
 import org.junit.jupiter.api.Test;
 
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EmpleadoIT {
