fcec9f20966bfe53a324a1375a3dbf9316c1d56a
rroca <r.roca@alumnos.upm.es>
2018-10-18 18:45:25 +0200

    Modifico RestauranteBusinessController. Utilizo ahora el Builder para crear la entidad Restaurante.

--- src/main/java/api/businessController/RestauranteBusinessController.java
+++ src/main/java/api/businessController/RestauranteBusinessController.java
@@ -1,16 +1,28 @@
 package api.businessController;
 
+import api.daos.DaoFactory;
 import api.dtos.RestauranteDto;
+import api.entities.Carta;
 import api.entities.Restaurante;
+import api.exceptions.NotFoundException;
 
 public class RestauranteBusinessController {
 
     public String create(RestauranteDto restauranteDto) {
+        /*
         Restaurante restaurante = new Restaurante();
         restaurante.setNombre(restauranteDto.getNombre());
         restaurante.setDireccion(restauranteDto.getDireccion());
         restaurante.setTipo(restauranteDto.getTipo());
+        */
+        restauranteDto.getCartaId();
+        Carta carta = null;
+        if (restauranteDto.getUserId() != null) {
+            carta = DaoFactory.getFactory().getCartaDao().read(restauranteDto.getCartaId())
+                    .orElseThrow(() -> new NotFoundException("Carta (" + restauranteDto.getCartaId() + ")"));
+        }
 
+        DaoFactory.getFactory().getRestauranteDao().save(restaurante);
         return restaurante.getId();
     }
 
